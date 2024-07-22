6fbabb3678a7fe0d003ed050eeca5a02b4a3eab4
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-07-22 18:38:46 +0000

    memoryallocations

---
+++ Basic/03.js
@@ -0,0 +1,15 @@
+//let number="222asdad"
+
+//console.log(typeof (number));
+
+//let valueInNumber = Number(number)
+
+//console.log(valueInNumber);
+
+// Nan = not in number 
+
+
+let isLoggedIn = 0
+
+let bool = Boolean(isLoggedIn)
+console.log(bool);
---
+++ Basic/memory.js
@@ -0,0 +1,17 @@
+let myname ="akshit"
+
+let anothername = myname 
+anothername = "jay"
+
+console.log(myname);
+console.log(anothername);
+
+
+let userOne={
+    email :"akshir@gmail.com",
+    upi  :"avbsx@upi",
+}
+
+let userTwo = userOne
+console.log(userOne);
+ userTwo.email="jaysdhau@gmail.com";
