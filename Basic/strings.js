2143c1fa13509c9b02b1eaf27b1d6964dba42485
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-07-24 18:13:36 +0000

    strings

--- Basic/strings.js
+++ Basic/strings.js
@@ -6,3 +6,11 @@ console.log(`hello my name is ${name} here the my count is ${count}`);
 
 const newString = name.substring(0,4)
 console.log(newString);
+
+const url="https://akshit.com/akshit%20shiroya"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sundar'));
+
+console.log(url.split('----'));
