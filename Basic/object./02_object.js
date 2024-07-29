c7bde8a14038cf0ca10874cd8394dcbf99b677c3
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-07-29 11:25:42 +0000

    object

---
+++ Basic/object./02_object.js
@@ -0,0 +1,25 @@
+//const tinderUser = new Object() // One singlrton object
+
+const newtinder = {} // non singleton object
+
+newtinder.name="akashier"
+newtinder.id=1212
+newtinder.isLoggedIn = false
+
+//console.log(newtinder);
+
+const newUser={
+    userfullname:{
+        usertd : {
+            firstname : "akshit",
+            lastname : "shiroya",
+
+            userdata:{
+                name : "jay shah",
+                surname :"sahsh"
+            }
+
+        }
+    }
+}
+//console.log(newUser.userdata);
