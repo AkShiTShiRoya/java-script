07909e2d624b992f31989c090058b6dfcfe05949
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-07-29 04:38:09 +0000

    dates

---
+++ Basic/dates_injs.js
@@ -0,0 +1,19 @@
+//dates
+ let myDate = new Date()
+
+//  console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleDateString());
+
+
+// let createdDate = new Date(2023 , 0 , 23)
+// let mycreatedDate = new Date(2023 , 0, 23, 5,3)
+// console.log(createdDate , mycreatedDate); 
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
