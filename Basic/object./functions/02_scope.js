367306889142c33d9b810ce9e7c78e02fa8f69a5
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-08-06 12:08:38 +0000

    functions

--- Basic/object./functions/02_scope.js
+++ Basic/object./functions/02_scope.js
@@ -1,11 +1,50 @@
-let a = 300
+// let a = 300
 
-if(true){
-    let a = 10
-    const b =20
+// if(true){
+//     let a = 10
+//     const b =20
 
-    console.log("inner values is :",a);
+//     console.log("inner values is :",a);
+    
+// }
+
+// console.log(a);
+
+function one() {
+    const username = "akshit"
+
+    function two(){
+        const website ="google.com"
+        console.log(username,website);
+        
+    }
+    two()
+}
+//one()
+
+if (true){
+    const username ="akshit shiroya"
+    if(username === "akshit shiroya"){
+        const website="  google.com"
+       // console.log(username + website);
+        
+    }
+    // console.log(username);
     
 }
+// console.log(username);
 
-console.log(a);
+
+
+//+++++++++++++++++++++++ interesting +++++++++++++++++++++
+
+function addone (number){
+    return number +1
+}
+number(6)
+
+const addTwo=function(number){
+    return number +2
+
+}
+addTwo(6)
