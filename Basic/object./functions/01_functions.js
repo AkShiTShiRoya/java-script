f7be7850cfbe8fa2cf8300a7720b9eac011b135a
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-08-06 06:32:34 +0000

    functions

---
+++ Basic/object./functions/01_functions.js
@@ -0,0 +1,42 @@
+// function Name(){
+//     console.log("jay shree ram");
+//     console.log("jay shree ram");
+//     console.log("jay shree ram");
+//     console.log("jay shree ram");
+//     console.log("jay shree ram");
+    
+// }
+// Name()
+
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+    
+// }
+//  addTwoNumbers(2,3)
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result=addTwoNumbers(3,31)
+
+// console.log("Result :",result);
+
+// function loginUserMessage(username){
+//     return `${username} just logged in the system`
+// }
+
+// console.log(loginUserMessage("akshit"))
+
+
+function loginUserMessage(username){
+    if(username === undefined){
+        console.log("Please enter the username");
+        return
+    }
+    return `${username} just loggged in `
+}
+
+console.log(loginUserMessage())
