13852dd9290bd3c519e1add53832724414be671c
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-08-08 16:48:28 +0000

    IIFE Functions

---
+++ Basic/object./functions/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function  chai(){
+    // named IIFE
+    console.log(`Databasse connected `);
+    
+})();
+
+((name) =>{
+    console.log(`Database esthablished${name}`);
+    
+})("akshit ")
