fef5e7d22621cc985c3680065b40189946d5fd95
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2024-08-22 17:51:15 +0000

    if/ else

---
+++ control_flow/01.js
@@ -0,0 +1,32 @@
+// if
+const tempeartuew = 41
+
+
+// if( tempeartuew < 50){
+//     console.log("less than 50");
+    
+// }
+// console.log("tempearture is greter than 50");
+const score =200
+
+// if(score > 100){
+//     let power ="run"
+//     console.log(`user power is :${power}`);
+    
+// }
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedINFromGoogle = false
+const loggedFromEmail = true
+
+if(userLoggedIn && debitcard){
+    console.log("alllow to aces");
+    
+}
+
+if(loggedINFromGoogle || loggedFromEmail){
+    console.log("user logged in");
+    
+}
