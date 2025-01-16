78bef9af8217b4acfbc0b2d46b1552d63f6f0c44
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2025-01-16 11:48:36 +0000

    setTimeoutandclosures

--- setTimeoutandclosures/01.js
+++ setTimeoutandclosures/01.js
@@ -1,10 +1,35 @@
-function x(){
-    for(let x=1 ; x<=5 ; x++){
-        setTimeout ( function(){
-            console.log(x);
+// function x(){
+//     for(var x=1 ; x<=5 ; x++){
+//         setTimeout ( function(){
+//             console.log(x);
             
-        }, x*1000
-    );
+//         }, x*1000
+//     );
+//     }
+
+//     console.log("Akshit......");
+    
+// }
+// x();
+
+
+
+// fix the problems 
+
+function x(){
+
+
+        for(var x=1 ; x<=5 ; x++){
+            function close(x){
+            setTimeout ( function(){
+                console.log(x);
+                
+            }, x*1000
+        );
+
+    }
+    close(x);
+
     }
 
     console.log("Akshit......");
