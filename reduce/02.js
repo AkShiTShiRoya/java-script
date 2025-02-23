cc3466b0f9fbbbd3fc841abea71e12e8bd80e4dd
Akshit Shiroya <142192945+AkShiTShiRoya@users.noreply.github.com>
2025-02-23 06:16:18 +0000

    objects

--- reduce/02.js
+++ reduce/02.js
@@ -14,6 +14,10 @@ const user=[
 //     { firstname : "om", lastname: "patel" ,  age : 23},
 //     { firstname : "john", lastname: "carter" ,  age : 45},
 //     { firstname : "harsh", lastname: "vala" ,  age : 75},
+//     { firstname : "dhurvish", lastname: "shah" ,  age : 33},//     { firstname : "Akshit", lastname: "patel" ,  age : 23},
+//     { firstname : "om", lastname: "patel" ,  age : 23},
+//     { firstname : "john", lastname: "carter" ,  age : 45},
+//     { firstname : "harsh", lastname: "vala" ,  age : 75},
 //     { firstname : "dhurvish", lastname: "shah" ,  age : 33},
     
 // ];
